808b36389e23ca6f98f8d9edbb59bca70efc879a
Hector van der Aa <hector@h3cx.dev>
2025-10-25 16:24:36 +0200

    Basic Raylib window with text and rendering loop

--- src/main.c
+++ src/main.c
@@ -3,15 +3,20 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
+  SetConfigFlags(FLAG_WINDOW_RESIZABLE);
   InitWindow(720, 480, (char *)"SpiderType");
-  while (!IsWindowReady())
-    ;
+  while (!IsWindowReady()) {
+  };
+  SetWindowMaxSize(3480, 2160);
+  SetWindowMinSize(200, 100);
+  SetTargetFPS(60);
   printf("Window ready");
-  BeginDrawing();
-  DrawRectangle(0, 0, 720, 480, WHITE);
-  DrawText((char *)"SpiderType", 10, 10, 20, BLACK);
-  EndDrawing();
-  while (true)
-    ;
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    DrawRectangle(0, 0, GetRenderWidth(), GetRenderHeight(), WHITE);
+    DrawText((char *)"SpiderType", 10, 10, 20, BLACK);
+    EndDrawing();
+  }
+  CloseWindow();
   return EXIT_SUCCESS;
 }
