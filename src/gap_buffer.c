43b91f9cdbd280517431f1997773e502347b4c27
Hector van der Aa <hector@h3cx.dev>
2025-10-27 23:00:05 +0000

    Added comments to gap_buffer.c

--- src/gap_buffer.c
+++ src/gap_buffer.c
@@ -3,6 +3,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Pretty useless function but exists just in case
+//
+// It sets all indexes to -1 an creates a single char wide data arry that simply
+// gets a null Termination
+//
+// This function will most likely break if then used with any sort of other
+// functions so yeah, pretty useless
 gap_buffer *GbInitEmptyBuffer() {
   gap_buffer *buffer = malloc(sizeof(gap_buffer));
   buffer->cursor_start = -1;
@@ -13,6 +20,9 @@ gap_buffer *GbInitEmptyBuffer() {
   return buffer;
 }
 
+// Inits a buffer to a passed size, the whole buffer is a cursor_start
+//
+// Should be self explanatory
 gap_buffer *GbInitBuffer(long req_size) {
   gap_buffer *buffer = malloc(sizeof(gap_buffer));
   buffer->cursor_start = 0;
@@ -23,6 +33,9 @@ gap_buffer *GbInitBuffer(long req_size) {
   return buffer;
 }
 
+// Inits a flushed buffer using a passed string
+//
+// Again should be self explanatory
 gap_buffer *GbInitFlushBufferFromString(char *input) {
   gap_buffer *buffer = malloc(sizeof(gap_buffer));
   buffer->cursor_start = -1;
@@ -35,11 +48,15 @@ gap_buffer *GbInitFlushBufferFromString(char *input) {
   return buffer;
 }
 
+// Destroys things
 void GbDestroy(gap_buffer *buffer) {
   free(buffer->data);
   free(buffer);
 }
 
+// Checks if a buffer is in its flushed state
+//
+// Flushed state meaning both cursor indexes are at -1
 bool GbIsFlushed(gap_buffer *buffer) {
   if (buffer->cursor_start == -1 && buffer->cursor_end == -1) {
     return true;
@@ -47,10 +64,12 @@ bool GbIsFlushed(gap_buffer *buffer) {
   return false;
 }
 
+// Returns the cursor width
 long GbCursorSize(gap_buffer *buffer) {
   return (buffer->cursor_end - buffer->cursor_start + 1);
 }
 
+// Returns the stored text length
 long GbTextSize(gap_buffer *buffer) {
   if (!GbIsFlushed(buffer)) {
     return (buffer->end - buffer->cursor_end + buffer->cursor_start);
@@ -58,6 +77,7 @@ long GbTextSize(gap_buffer *buffer) {
   return (buffer->end + 1);
 }
 
+// Copies buffer
 gap_buffer *GbCopyBuffer(gap_buffer *buffer) {
   gap_buffer *bufferCopy = malloc(sizeof(gap_buffer));
   bufferCopy->cursor_start = buffer->cursor_start;
@@ -69,6 +89,10 @@ gap_buffer *GbCopyBuffer(gap_buffer *buffer) {
   return bufferCopy;
 }
 
+// Flushes buffer by first shifting buffer end data to concatenate it together
+// then realloc to trim the buffer
+//
+//  TODO : Confirm if memcpy and realloc implementation is correct and safe
 bool GbFlushBuffer(gap_buffer *buffer) {
   if (GbIsFlushed(buffer)) {
     return false;
@@ -84,7 +108,10 @@ bool GbFlushBuffer(gap_buffer *buffer) {
   return true;
 }
 
-// Doesn't work for resizing to smaller gap, code review
+// Resizes cursor to given size
+//
+// Confrmed working to gap_size > current_gap_size
+//  TODO : Fix for gap_size < current_gap_size, currently broken
 void GbResizeCursor(gap_buffer *buffer, long gap_size) {
   char *endData = malloc((buffer->end - buffer->cursor_end + 1));
   strcpy(endData, &buffer->data[buffer->cursor_end + 1]);
@@ -96,6 +123,7 @@ void GbResizeCursor(gap_buffer *buffer, long gap_size) {
   buffer->data[buffer->end + 1] = '\0';
 }
 
+// Inserts cursor to a given position in a flushed buffer
 void GbInsertCursor(gap_buffer *buffer, long insert_position, long gap_size) {
   if (!GbIsFlushed(buffer)) {
     GbFlushBuffer(buffer);
@@ -116,6 +144,8 @@ void GbInsertCursor(gap_buffer *buffer, long insert_position, long gap_size) {
   }
 }
 
+// Prints alot of stuff, doesn't really need messing with unless its to add more
+// debug info
 void GbPrintBufferDebug(gap_buffer *buffer) {
   printf((char *)"\nCursor Start: ");
   printf("%ld\n", buffer->cursor_start);
@@ -162,6 +192,7 @@ void GbPrintBufferDebug(gap_buffer *buffer) {
   }
 }
 
+// Insert char at cursor_start
 bool GbInsertChar(gap_buffer *buffer, char input) {
   if (GbCursorSize(buffer) > 0) {
     buffer->data[buffer->cursor_start] = input;
@@ -171,6 +202,8 @@ bool GbInsertChar(gap_buffer *buffer, char input) {
   return false;
 }
 
+// Move cursor left by shifting single char from cursor_start - 1 to cursor_end
+// then shifting cursor indexes by -1
 bool GbMoveLeft(gap_buffer *buffer) {
   if (buffer->cursor_start > 0) {
     buffer->data[buffer->cursor_end] = buffer->data[buffer->cursor_start - 1];
@@ -181,6 +214,8 @@ bool GbMoveLeft(gap_buffer *buffer) {
   return false;
 }
 
+// Move cursor right by shifting single char from cursor_end + 1 to cursor_start
+// then shifting cursor indexes by +1
 bool GbMoveRight(gap_buffer *buffer) {
   if (buffer->cursor_end < buffer->end) {
     buffer->data[buffer->cursor_start] = buffer->data[buffer->cursor_end + 1];
@@ -191,6 +226,7 @@ bool GbMoveRight(gap_buffer *buffer) {
   return false;
 }
 
+// Deletes char right of cursor_end by shifting cursor_end by +1
 bool GbDelete(gap_buffer *buffer) {
   if (buffer->cursor_end < buffer->end) {
     buffer->cursor_end++;
@@ -199,6 +235,7 @@ bool GbDelete(gap_buffer *buffer) {
   return false;
 }
 
+// Delets char left of cursor_start by moving cursor_start by -1
 bool GbBackspace(gap_buffer *buffer) {
   if (buffer->cursor_start > 0) {
     buffer->cursor_start--;
@@ -207,6 +244,8 @@ bool GbBackspace(gap_buffer *buffer) {
   return false;
 }
 
+// Returns concatenated string of buffer without flushing the buffer in the case
+// of an open buffer to avoid large memory operations
 char *GbConcatenate(gap_buffer *buffer) {
   if (!GbIsFlushed(buffer)) {
 
