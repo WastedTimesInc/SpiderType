cf369e8806ddce240ef06876613194d86827e54c
Hector van der Aa <hector@h3cx.dev>
2025-10-28 10:58:31 +0000

    Segmented LoadStringFromFile

--- src/main.c
+++ src/main.c
@@ -3,12 +3,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+char *LoadStringFromFile(FILE *f, char *path) {
+  f = fopen(path, "rw");
+  fseek(f, 0, SEEK_END);
+  long fsize = ftell(f);
+  char *returnVal = malloc((fsize + 1) * sizeof(char));
+  fseek(f, 0, SEEK_SET);
+  fscanf(f, "%s", returnVal);
+  return returnVal;
+}
+
 int main(int argc, char *argv[]) {
-  gap_buffer *buffer = GbInitFlushBufferFromString((char *)"Hello World");
-  GbPrintBufferDebug(buffer);
-  GbInsertCursor(buffer, 0, 5);
-  GbPrintBufferDebug(buffer);
-  /*SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+  SetConfigFlags(FLAG_WINDOW_RESIZABLE);
   InitWindow(720, 480, (char *)"SpiderType");
   while (!IsWindowReady()) {
   };
@@ -16,29 +22,29 @@ int main(int argc, char *argv[]) {
   SetWindowMinSize(200, 100);
   SetTargetFPS(60);
   printf("Window ready\n");
-  int tot_line =0;
-  int current_line =0;
+  int tot_line = 0;
+  int current_line = 0;
   char *concat;
   int mode = 0;
   int fsize = 0;
   gap_buffer testBuff;
-  int x_offset =10;
-  FILE* f = NULL;
-  if(argc>1){
-    f = fopen(argv[1],"rw");
-    fseek(f,0,SEEK_END);
-    fsize=ftell(f);
+  int x_offset = 10;
+  FILE *f = NULL;
+  if (argc > 1) {
+    f = fopen(argv[1], "rw");
+    fseek(f, 0, SEEK_END);
+    fsize = ftell(f);
     GapBufferInit(&testBuff, fsize);
-    testBuff.cursor_start+=fsize-1;
-    fseek(f,0,SEEK_SET);
-    fscanf(f,"%s",testBuff.data);
-    for(int i=0;i<fsize;i++){
-      if(testBuff.data[i]=='\n'){
+    testBuff.cursor_start += fsize - 1;
+    fseek(f, 0, SEEK_SET);
+    fscanf(f, "%s", testBuff.data);
+    for (int i = 0; i < fsize; i++) {
+      if (testBuff.data[i] == '\n') {
         tot_line++;
       }
     }
 
-  }else{
+  } else {
     GapBufferInit(&testBuff, 10);
   }
   while (!WindowShouldClose()) {
@@ -51,49 +57,50 @@ int main(int argc, char *argv[]) {
       GapBufferDelete(&testBuff);
       break;
     case KEY_LEFT:
-      if(current_line!=0&&testBuff.data[testBuff.cursor_start]=='\n'){
-      current_line--;
+      if (current_line != 0 && testBuff.data[testBuff.cursor_start] == '\n') {
+        current_line--;
       }
       GapBufferMoveLeft(&testBuff, 1);
       break;
     case KEY_RIGHT:
-      if(current_line!=tot_line&&testBuff.data[testBuff.cursor_start]=='\n'){
+      if (current_line != tot_line &&
+          testBuff.data[testBuff.cursor_start] == '\n') {
         current_line++;
       }
       GapBufferMoveRight(&testBuff, 1);
       break;
     case KEY_ENTER:
-      GapBufferInsert(&testBuff,'\n');
+      GapBufferInsert(&testBuff, '\n');
       tot_line++;
-      x_offset=MeasureText(TextFormat("%i",tot_line),10);
+      x_offset = MeasureText(TextFormat("%i", tot_line), 10);
       current_line++;
       break;
     case KEY_LEFT_CONTROL:
-      mode +=1;
+      mode += 1;
     default:
       if (pressed != 0) {
-        switch (mode){
-            case 1:
-              switch(pressed){ //Switch for the CTRL+KEY bindings
-                case 's':
-                  if(f==NULL){
-                    //TODO Handle queriying the user for a new file
-                  }else{
-                    concat = GapBufferConcatenate(&testBuff);
-                    fprintf(f,"%s",concat);
-                    free(concat);
-                    mode-=1;
-                  }
-                  break;
-                default:
-                  mode-=1;
-                  break;
-              }
-              break;
-            default:
-              GapBufferInsert(&testBuff, (char)pressed);
-              break;
+        switch (mode) {
+        case 1:
+          switch (pressed) { // Switch for the CTRL+KEY bindings
+          case 's':
+            if (f == NULL) {
+              // TODO Handle queriying the user for a new file
+            } else {
+              concat = GapBufferConcatenate(&testBuff);
+              fprintf(f, "%s", concat);
+              free(concat);
+              mode -= 1;
+            }
+            break;
+          default:
+            mode -= 1;
+            break;
           }
+          break;
+        default:
+          GapBufferInsert(&testBuff, (char)pressed);
+          break;
+        }
       }
       break;
     }
@@ -102,17 +109,20 @@ int main(int argc, char *argv[]) {
     DrawRectangle(0, 0, GetRenderWidth(), GetRenderHeight(), WHITE);
     DrawText((char *)"SpiderType", 10, 10, 20, BLACK);
     concat = GapBufferConcatenate(&testBuff);
-    DrawText(concat, 10+x_offset, 30, 20, BLACK);
+    DrawText(concat, 10 + x_offset, 30, 20, BLACK);
 
-    for(int i =0;i<=tot_line;i++){
-      if(i!=current_line){
-        DrawText(TextFormat("%i",((i-current_line)*(i>current_line)+(current_line-i)*(i<current_line))),0,35+i*22,10,BLACK);
+    for (int i = 0; i <= tot_line; i++) {
+      if (i != current_line) {
+        DrawText(TextFormat("%i", ((i - current_line) * (i > current_line) +
+                                   (current_line - i) * (i < current_line))),
+                 0, 35 + i * 22, 10, BLACK);
       }
     }
-    DrawText(TextFormat("%i",current_line),0,35+current_line*22,10,RED);
+    DrawText(TextFormat("%i", current_line), 0, 35 + current_line * 22, 10,
+             RED);
     free(concat);
     EndDrawing();
   }
-  CloseWindow();*/
+  CloseWindow();
   return EXIT_SUCCESS;
 }
